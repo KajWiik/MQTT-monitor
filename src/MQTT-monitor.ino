48c0cfa55e3cb14d3498dabaaed730d51a7efa17
Kaj Wiik <kjwiik@iki.fi>
2018-01-14 01:45:48 +0200

    working without gsm

--- src/MQTT-monitor.ino
+++ src/MQTT-monitor.ino
@@ -35,93 +35,156 @@
 
 #include <TinyGsmClient.h>
 #include <PubSubClient.h>
-#include <OneWire.h>
+//#include <OneWire.h>
 #include <SimpleDHT.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+
+// Edit BME280_ADDRESS (0x76)
+#include <Adafruit_BME280.h>
 
 // Your GPRS credentials
 // Leave empty, if missing user or pass
-const char apn[]  = "";
+const char apn[]  = "internet";
 const char user[] = "";
 const char pass[] = "";
+const char mqtt_user[] = "valpuri";
+const char mqtt_pass[] = "ammu1001";
 
 // Use Hardware Serial on Mega, Leonardo, Micro
-#define SerialAT Serial1
+//#define SerialAT Serial1
 
 // or Software Serial on Uno, Nano
-//#include <SoftwareSerial.h>
-//SoftwareSerial SerialAT(2, 3); // RX, TX
+#include <SoftwareSerial.h>
+SoftwareSerial SerialAT(2, 3); // RX, TX
 
 TinyGsm modem(SerialAT);
 TinyGsmClient client(modem);
 PubSubClient mqtt(client);
+SimpleDHT22 dht22;
+Adafruit_BME280 bme;
 
-const char* broker = "test.mosquitto.org";
+const char* broker = "kw.dy.fi";
 
-const char* topicLed = "GsmClientTest/led";
-const char* topicInit = "GsmClientTest/init";
-const char* topicLedStatus = "GsmClientTest/ledStatus";
+//const String topicLammitin = "Karttula/Lammitin/";
+//const String topicLammitinStatus = "Karttula/LammitinStatus/";
+const char* topicHeater = "Karttula/Lammitin/";
+const char* topicHeaterStatus = "Karttula/LammitinStatus/";
+//const String topicInit = "Karttula/init/";
+const char* topicInit = "Karttula/init/";
+const String topicOlohuone = "Karttula/Olohuone/";
+const String topicMakuuhuone = "Karttula/Makuuhuone/";
+const String topicVierashuone = "Karttula/Vierashuone/";
+const String topicWC = "Karttula/WC/";
+//const String topicWClattia = "Karttula/WClattia/";
 
-#define LED_PIN 13
-int ledStatus = LOW;
+//#define Olohuone 10
+#define Makuuhuone 4
+#define WC 5
+#define Vierashuone 6
+
+#define HEATER_PIN 13
+int HeaterStatus = LOW;
 
 long lastReconnectAttempt = 0;
+unsigned long lastrun = millis();
+int state = 0;
+//#define PERIOD 5*60*1000
+#define PERIOD 3000
+
+bool bme_status;
+#define WIDTH 6
+#define PRECISION 1
+#define BUFLEN 36
+
+char charbuffer[BUFLEN];
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(HEATER_PIN, OUTPUT);
 
   // Set console baud rate
   Serial.begin(115200);
   delay(10);
 
   // Set GSM module baud rate
-  SerialAT.begin(115200);
+  SerialAT.begin(38400);
   delay(3000);
 
-  // Restart takes quite some time
-  // To skip it, call init() instead of restart()
-  Serial.println("Initializing modem...");
-  modem.restart();
+  if (0) {
+    // Restart takes quite some time
+    // To skip it, call init() instead of restart()
+    Serial.println("Initializing modem...");
+    modem.restart();
 
-  // Unlock your SIM card with a PIN
-  //modem.simUnlock("1234");
+    // Unlock your SIM card with a PIN
+    //modem.simUnlock("1234");
 
-  Serial.print("Waiting for network...");
-  if (!modem.waitForNetwork()) {
-    Serial.println(" fail");
-    while (true);
+    Serial.print("Waiting for network...");
+    if (!modem.waitForNetwork()) {
+      Serial.println(" fail");
+      while (true);
+    }
+    Serial.println(" OK");
+
+    Serial.print("Connecting to ");
+    Serial.print(apn);
+    if (!modem.gprsConnect(apn, user, pass)) {
+      Serial.println(" fail");
+      while (true);
+    }
+    Serial.println(" OK");
+
+    // MQTT Broker setup
+    mqtt.setServer(broker, 1883);
+    mqtt.setCallback(mqttCallback);
   }
-  Serial.println(" OK");
 
-  Serial.print("Connecting to ");
-  Serial.print(apn);
-  if (!modem.gprsConnect(apn, user, pass)) {
-    Serial.println(" fail");
-    while (true);
-  }
-  Serial.println(" OK");
-
-  // MQTT Broker setup
-  mqtt.setServer(broker, 1883);
-  mqtt.setCallback(mqttCallback);
+  bme_status = bme.begin();
+  
 }
 
 boolean mqttConnect() {
   Serial.print("Connecting to ");
   Serial.print(broker);
-  if (!mqtt.connect("GsmClientTest")) {
+  if (!mqtt.connect("Karttula", mqtt_user, mqtt_pass)) {
     Serial.println(" fail");
     return false;
   }
   Serial.println(" OK");
-  mqtt.publish(topicInit, "GsmClientTest started");
-  mqtt.subscribe(topicLed);
+  mqtt.publish(topicInit, "Karttula started");
+  mqtt.subscribe(topicHeater);
   return mqtt.connected();
 }
 
 void loop() {
 
-  if (mqtt.connected()) {
-    mqtt.loop();
+  //  if (mqtt.connected()) {
+  if (1) {
+    if (millis() > (lastrun + PERIOD)) {
+      switch(state) {
+      case 0:
+	senddht22(Makuuhuone, topicMakuuhuone);
+	state++;
+	break;
+
+      case 1:
+	sendbme(topicOlohuone);
+	state++;
+	break;
+
+      case 2:
+	senddht22(WC, topicWC);
+	state++;
+	break;
+
+      case 3:
+	// Foo
+	state = 0;
+	lastrun = millis();
+      }
+    }
+    delay(1000);
+    //    mqtt.loop();
   } else {
     // Reconnect every 10 seconds
     unsigned long t = millis();
@@ -132,8 +195,16 @@ void loop() {
       }
     }
   }
-
 }
+
+/* { */
+/*  lastrun = millis(); */
+/*  String pubString = "{"report":{"light": "" + String(lightRead,1) + ""}}"; */
+/*  pubString.toCharArray(message_buff, pubString.length()+1); */
+/*  //Serial.println(pubString); */
+/*  client.publish("io.m2m/arduino/lightsensor", message_buff); */
+/* } */
+/*     mqtt.loop(); */
 
 void mqttCallback(char* topic, byte* payload, unsigned int len) {
   Serial.print("Message arrived [");
@@ -143,9 +214,51 @@ void mqttCallback(char* topic, byte* payload, unsigned int len) {
   Serial.println();
 
   // Only proceed if incoming message's topic matches
-  if (String(topic) == topicLed) {
-    ledStatus = !ledStatus;
-    digitalWrite(LED_PIN, ledStatus);
-    mqtt.publish(topicLedStatus, ledStatus ? "1" : "0");
+  if (String(topic) == topicHeater) {
+    HeaterStatus = payload[0] == '0' ? LOW : HIGH; 
+    digitalWrite(HEATER_PIN, HeaterStatus);
+    mqtt.publish(topicHeaterStatus, HeaterStatus ? "1" : "0");
   }
 }
+
+void senddht22(int pinDHT22, String topic) {
+  float temperature = 0;
+  float humidity = 0;
+  String message;
+  
+  int err = SimpleDHTErrSuccess;
+  //  Serial.print("In DHT ");
+  //Serial.println(topic);
+  //Serial.println(pinDHT22);
+
+  if ((err = dht22.read2(pinDHT22, &temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    delay(2000);
+    Serial.println("DHT init failed");
+    return;
+  }
+  message = topic + "T/" + String(temperature, 1);
+  message.toCharArray(charbuffer, BUFLEN);
+  Serial.println(charbuffer);
+  message = topic + "RH/" + String(humidity, 1);
+  message.toCharArray(charbuffer, BUFLEN);
+  Serial.println(charbuffer);
+}
+  
+void sendbme(String topic) {
+  String message;
+
+  if (bme_status) {
+    message = topic + "T/" + String(bme.readTemperature(), 1);
+    message.toCharArray(charbuffer, BUFLEN);
+    Serial.println(charbuffer);
+    message = topic + "RH/" + String(bme.readHumidity(), 1);
+    message.toCharArray(charbuffer, BUFLEN);
+    Serial.println(charbuffer);
+    message = topic + "P/" + String(bme.readPressure()/100, 2);
+    message.toCharArray(charbuffer, BUFLEN);
+    Serial.println(charbuffer);
+  }
+}
+  
+
+  //  mqtt.publish(topic, xxx);
