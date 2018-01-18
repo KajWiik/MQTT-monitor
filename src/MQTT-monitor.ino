cbf7cd19ff9485a33dd40edf70c63c5ef81db32f
Kaj Wiik <kjwiik@iki.fi>
2018-01-18 17:01:57 +0200

    Full version, 80% of program space!

--- src/MQTT-monitor.ino
+++ src/MQTT-monitor.ino
@@ -26,55 +26,102 @@
  **************************************************************/
 
 // Select your modem:
-// #define TINY_GSM_MODEM_SIM800
-// #define TINY_GSM_MODEM_SIM900
-// #define TINY_GSM_MODEM_A6
-// #define TINY_GSM_MODEM_A7
 #define TINY_GSM_MODEM_M590
-// #define TINY_GSM_MODEM_ESP8266
 
 #include <TinyGsmClient.h>
 #include <PubSubClient.h>
-#include <OneWire.h>
+
 #include <SimpleDHT.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+
+// Edit BME280_ADDRESS (0x76)
+#include <Adafruit_BME280.h>
+
+#include <Adafruit_SleepyDog.h>
 
 // Your GPRS credentials
 // Leave empty, if missing user or pass
-const char apn[]  = "";
+const char apn[]  = "internet";
 const char user[] = "";
 const char pass[] = "";
+const char mqtt_user[] = "valpuri";
+const char mqtt_pass[] = "ammu1001";
 
 // Use Hardware Serial on Mega, Leonardo, Micro
-#define SerialAT Serial1
+//#define SerialAT Serial1
 
 // or Software Serial on Uno, Nano
-//#include <SoftwareSerial.h>
-//SoftwareSerial SerialAT(2, 3); // RX, TX
-
+#include <SoftwareSerial.h>
+SoftwareSerial SerialAT(2, 3); // RX, TX
 TinyGsm modem(SerialAT);
 TinyGsmClient client(modem);
 PubSubClient mqtt(client);
 
-const char* broker = "test.mosquitto.org";
+SimpleDHT22 dht22;
 
-const char* topicLed = "GsmClientTest/led";
-const char* topicInit = "GsmClientTest/init";
-const char* topicLedStatus = "GsmClientTest/ledStatus";
+Adafruit_BME280 bme;
 
-#define LED_PIN 13
-int ledStatus = LOW;
+const char broker[] = "kw.dy.fi";
+
+const char topicHeater[] = "Karttula/Lammitin/";
+const char topicHeaterStatus[] = "Karttula/LammitinStatus/";
+const char topicInit[] = "Karttula/init/";
+const char topicOlohuone[] = "Karttula/Olohuone/";
+const char topicMakuuhuone[] = "Karttula/Makuuhuone/";
+const char topicVierashuone[] = "Karttula/Vierashuone/";
+const char topicWC[] = "Karttula/WC/";
+
+const char tempprefix[] = "T/";
+const char humprefix[] = "RH/";
+const char pressprefix[] = "P/";
+
+#define Makuuhuone 4
+#define WC 5
+#define Vierashuone 6
+
+#define HEATER_PIN 7
+#define GSM_RESET_PIN 8
+
+int HeaterStatus = LOW;
 
 long lastReconnectAttempt = 0;
+unsigned long lastrun = millis();
+int state = 0;
+//#define PERIOD 5*60*1000
+#define PERIOD 3000
+
+bool bme_status;
+
+#define TOPICBUFLEN 36
+#define VALBUFLEN 10
+
+char topicbuffer[TOPICBUFLEN];
+char valbuffer[VALBUFLEN];
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
 
+  int countdownMS = Watchdog.enable(20000);
+  
+  pinMode(HEATER_PIN, OUTPUT);
+  pinMode(GSM_RESET_PIN, OUTPUT);
+
+  // Turn heater off by default
+  digitalWrite(HEATER_PIN, LOW);
+
+  // Reset GSM module
+  digitalWrite(GSM_RESET_PIN, LOW);
+  delay(100);
+  digitalWrite(GSM_RESET_PIN, HIGH);
+  delay(500);
+  digitalWrite(GSM_RESET_PIN, LOW);
+  
   // Set console baud rate
   Serial.begin(115200);
   delay(10);
 
   // Set GSM module baud rate
-  SerialAT.begin(115200);
+  SerialAT.begin(9600);
   delay(3000);
 
   // Restart takes quite some time
@@ -99,28 +146,56 @@ void setup() {
     while (true);
   }
   Serial.println(" OK");
-
+  
   // MQTT Broker setup
   mqtt.setServer(broker, 1883);
   mqtt.setCallback(mqttCallback);
+
+  bme_status = bme.begin();
+  Watchdog.reset();
 }
 
 boolean mqttConnect() {
   Serial.print("Connecting to ");
   Serial.print(broker);
-  if (!mqtt.connect("GsmClientTest")) {
+  if (!mqtt.connect("Karttula", mqtt_user, mqtt_pass)) {
     Serial.println(" fail");
     return false;
   }
   Serial.println(" OK");
-  mqtt.publish(topicInit, "GsmClientTest started");
-  mqtt.subscribe(topicLed);
+  mqtt.publish(topicInit, "Karttula started");
+  mqtt.subscribe(topicHeater);
   return mqtt.connected();
 }
 
 void loop() {
 
   if (mqtt.connected()) {
+    //  if (1) {
+    if (millis() > (lastrun + PERIOD)) {
+      switch(state) {
+      case 0:
+	senddht22(Makuuhuone, topicMakuuhuone);
+	state++;
+	break;
+
+      case 1:
+	sendbme(topicOlohuone);
+	state++;
+	break;
+
+      case 2:
+	senddht22(WC, topicWC);
+	state++;
+	break;
+
+      case 3:
+	// Foo
+	state = 0;
+	lastrun = millis();
+      }
+    }
+    delay(1000);
     mqtt.loop();
   } else {
     // Reconnect every 10 seconds
@@ -132,7 +207,6 @@ void loop() {
       }
     }
   }
-
 }
 
 void mqttCallback(char* topic, byte* payload, unsigned int len) {
@@ -143,9 +217,57 @@ void mqttCallback(char* topic, byte* payload, unsigned int len) {
   Serial.println();
 
   // Only proceed if incoming message's topic matches
-  if (String(topic) == topicLed) {
-    ledStatus = !ledStatus;
-    digitalWrite(LED_PIN, ledStatus);
-    mqtt.publish(topicLedStatus, ledStatus ? "1" : "0");
+  if (String(topic) == topicHeater) {
+    HeaterStatus = payload[0] == '0' ? LOW : HIGH; 
+    digitalWrite(HEATER_PIN, HeaterStatus);
+    mqtt.publish(topicHeaterStatus, HeaterStatus ? "1" : "0");
   }
 }
+
+void senddht22(int pinDHT22, char* topic) {
+  float temperature = 0;
+  float humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  size_t len = strlen(topic);
+
+  if ((err = dht22.read2(pinDHT22, &temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    delay(2000);
+    Serial.println("DHT init failed");
+    return;
+  }
+
+  strcpy(topicbuffer,topic);
+  
+  strcpy(topicbuffer + len, tempprefix);
+  dtostrf(temperature, 0, 1, valbuffer);
+  mqtt.publish(topicbuffer, valbuffer);
+  
+  strcpy(topicbuffer + len, humprefix);
+  dtostrf(humidity, 0, 1, valbuffer);
+  mqtt.publish(topicbuffer, valbuffer);
+}
+  
+void sendbme(char* topic) {
+  float temperature = 0;
+  float humidity = 0;
+  size_t len = strlen(topic);
+
+  if (bme_status) {
+    strcpy(topicbuffer,topic);
+  
+    strcpy(topicbuffer + len, tempprefix);
+    dtostrf(bme.readTemperature(), 0, 1, valbuffer);
+    mqtt.publish(topicbuffer, valbuffer);
+
+    strcpy(topicbuffer + len, humprefix);
+    dtostrf(bme.readHumidity(), 0, 1, valbuffer);
+    mqtt.publish(topicbuffer, valbuffer);
+
+    strcpy(topicbuffer + len, pressprefix);
+    dtostrf(bme.readPressure(), 0, 2, valbuffer);
+    mqtt.publish(topicbuffer, valbuffer);
+  }
+}
+// foo
+
+  //  mqtt.publish(topic, xxx);
